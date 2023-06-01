20b91042c778fdc8b94a7c8c0026a074305d2cd4
꧁༺☬๖ۣۜṨtͥevͣeͫภ☬༻꧂ <teve@ACERNITRO5>
2023-06-01 06:11:38 -0500

    Navbar Actualizacion

---
+++ appWebServisoft/Vista/Js/SiteMaster2.js
@@ -0,0 +1,7 @@
+﻿var toggleButton = document.getElementById("toggleButton");
+var cardElements = document.getElementById("cardElements");
+
+toggleButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevenir comportamiento predeterminado del botón
+    cardElements.classList.toggle("show-links");
+});
--- appWebServisoft/Vista/MasterCliente.Master.cs
+++ appWebServisoft/Vista/MasterCliente.Master.cs
@@ -22,6 +22,10 @@ namespace appWebServisoft.Vista
 
         }
 
+       
+       
+
+
 
 
 
