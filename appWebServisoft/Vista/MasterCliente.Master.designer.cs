7c49c336c1ddce573580d245b3e7e60d0290246b
꧁༺☬๖ۣۜṨtͥevͣeͫภ☬༻꧂ <teve@ACERNITRO5>
2023-06-09 10:45:09 -0500

    NavBarActualizado

--- appWebServisoft/Vista/MasterCliente.Master.designer.cs
+++ appWebServisoft/Vista/MasterCliente.Master.designer.cs
@@ -42,31 +42,13 @@ namespace appWebServisoft.Vista
         protected global::System.Web.UI.WebControls.Label lblUsuario;
 
         /// <summary>
-        /// Control TextBox2.
+        /// Control miImageButton.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
-
-        /// <summary>
-        /// Control Button1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton Button1;
-
-        /// <summary>
-        /// Control ddlOpciones.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlOpciones;
+        protected global::System.Web.UI.WebControls.ImageButton miImageButton;
 
         /// <summary>
         /// Control ContentPlaceHolder1.
